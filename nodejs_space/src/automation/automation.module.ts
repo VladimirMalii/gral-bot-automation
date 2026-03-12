d4e46ed0862cdd3707e9cd7b5c69b130525f0db6
VladimirMalii <burlacuv2001@gmail.com>
2026-03-12 14:09:53 +0200

    Update automation.module.ts

--- nodejs_space/src/automation/automation.module.ts
+++ nodejs_space/src/automation/automation.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AutomationController } from './automation.controller';
 import { AutomationService } from './automation.service';
 import { GralmedApiService } from './gralmed-api.service';
+import { AppointmentFinalizationService } from './appointment-finalization.service';
 
 @Module({
   controllers: [AutomationController],
-  providers: [AutomationService, GralmedApiService],
-  exports: [AutomationService, GralmedApiService]
+  providers: [AutomationService, GralmedApiService, AppointmentFinalizationService],
+  exports: [AutomationService, GralmedApiService, AppointmentFinalizationService]
 })
 export class AutomationModule {}
